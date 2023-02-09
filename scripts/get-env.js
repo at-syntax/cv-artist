8f068b2154b5c382dfd7175143f010dddf7df5a7
saikat-samanta <saikatsamanta737@gmail.com>
2023-02-09 10:02:49 +0530

    refactor: :fire: update .env config

--- scripts/get-env.js
+++ scripts/get-env.js
@@ -3,17 +3,11 @@ const { config } = require("dotenv");
 
 let data;
 try {
-  let result;
-  if (process.env.NODE_ENV === "production") {
-    result = config({
-      path: join(__dirname, `../.env`),
-    });
-  } else {
-    result = config({
-      path: join(__dirname, "../.env.local"),
-    });
-  }
-  data = result.parsed[process.argv[2]]; // Reads the specified environment variable from .env file
+  const { parsed } = config({
+    path: join(__dirname, `../.env`),
+  });
+
+  data = parsed[process.argv[2]]; // Reads the specified environment variable from .env file
 } catch {
   data = process.env[process.argv[2]]; // Reads the specified environment variable from process.env
 } finally {
